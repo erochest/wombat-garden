56de6d72fad5e00488da8bd80751ec9cfc04c9c7
Eric Rochester <eric.rochester@willowtreeapps.com>
2017-03-17 21:45:47 -0400

    Most of `wombat-garden init`.

--- src/WombatGarden/Actions/Init.hs
+++ src/WombatGarden/Actions/Init.hs
@@ -5,9 +5,62 @@ module WombatGarden.Actions.Init where
 
 
 import           Control.Error
-import qualified Data.Text     as T
+import           Control.Monad
+import           Data.Aeson           (encode)
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Text            as T
+import           System.Directory
+import           System.FilePath
+import           System.IO
+import           System.Process
+
+import           WombatGarden.Types
 
 
 initAction :: T.Text -> Int -> Int -> Int -> Double -> Double -> Int
            -> Script ()
-initAction _name _population _carryOver _newRandom _crossOver _mutation _depth = undefined
+initAction name population carryOver newRandom crossOver mutation
+           depth = do
+  scriptIO $ createDirectoryIfMissing True name'
+  scriptIO $ withCurrentDirectory name' $ runScript $ do
+    git_ "init" []
+    hub_ "create" []
+    scriptIO $ BL.writeFile (name' </> "params.json") $ encode params
+    scriptIO $ BL.writeFile (name' </> "data-log.json") mempty
+
+    -- TODO: generate first generation
+    void $ undefined
+
+    gitAdd "."
+    gitCommit "garden set up"
+    git_ "push" ["-u", "origin", "master"]
+  where
+    name' = T.unpack name
+    params = Parameters population carryOver newRandom crossOver
+                        mutation depth
+
+type ProcessOut = Script ( Maybe Handle, Maybe Handle, Maybe Handle
+                         , ProcessHandle
+                         )
+
+git :: String -> [String] -> ProcessOut
+git = command "git"
+
+git_ :: String -> [String] -> Script ()
+git_ cmd = void . git cmd
+
+gitAdd :: FilePath -> Script ()
+gitAdd = git_ "add" . pure
+
+gitCommit :: String -> Script ()
+gitCommit msg = git_ "commit" ["-m", msg]
+
+hub :: String -> [String] -> ProcessOut
+hub = command "hub"
+
+hub_ :: String -> [String] -> Script ()
+hub_ cmd = void . hub cmd
+
+command :: String -> String -> [String] -> ProcessOut
+command cmd0 cmd1 args =
+  scriptIO $ createProcess $ proc cmd0 $ cmd1 : args
--- src/WombatGarden/Types.hs
+++ src/WombatGarden/Types.hs
@@ -1,19 +1,46 @@
--- {-# LANGUAGE DeriveDataTypeable         #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 -- {-# LANGUAGE DeriveFunctor              #-}
--- {-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DeriveGeneric      #-}
 -- {-# LANGUAGE DeriveTraversable          #-}
 -- {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 -- {-# LANGUAGE OverloadedLists            #-}
 -- {-# LANGUAGE OverloadedStrings          #-}
 -- {-# LANGUAGE RankNTypes                 #-}
 -- {-# LANGUAGE RecordWildCards            #-}
--- {-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TemplateHaskell    #-}
 
 
 module WombatGarden.Types where
 
 
--- import           Control.Lens
--- import           Data.Data
+import           Control.Lens
+import           Data.Data
 -- import qualified Data.Text              as T
--- import           GHC.Generics           hiding (to)
+import           Data.Aeson
+import           Data.Aeson.Types
+import qualified Data.List        as L
+import           GHC.Generics     hiding (to)
+
+
+data Parameters
+  = Parameters
+  { _paramPopulation :: !Int
+  , _paramCarryOver  :: !Int
+  , _paramNewRandom  :: !Int
+  , _paramCrossOver  :: !Double
+  , _paramMutation   :: !Double
+  , _paramDepth      :: !Int
+  } deriving (Show, Eq, Generic, Data, Typeable)
+
+makeClassy ''Parameters
+
+
+paramOptions :: Options
+paramOptions = defaultOptions { fieldLabelModifier = L.drop 6 }
+
+instance ToJSON Parameters where
+  toJSON     = genericToJSON     paramOptions
+  toEncoding = genericToEncoding paramOptions
+
+instance FromJSON Parameters where
+  parseJSON = genericParseJSON paramOptions
